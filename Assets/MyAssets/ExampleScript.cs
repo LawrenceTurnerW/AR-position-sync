db587f30e3b7abf04aa04cb234c1dd50e16a97f1
LawrenceTurnerW <watawata.takatan@gmail.com>
2023-10-04 23:21:04 +0900

    座標設定時の目印のためのブレハブを作成

---
+++ Assets/MyAssets/ExampleScript.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ExampleScript : MonoBehaviour
+{
+    public GameObject point1; // 手に近いほうの座標を記録
+    public GameObject point2; // 手から遠いほうの座標を記録
+
+    void Start()
+    {
+    }
+}
