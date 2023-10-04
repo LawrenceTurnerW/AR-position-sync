3b015ce3ae4234d59c2235e851172bcc105d9f4a
LawrenceTurnerW <watawata.takatan@gmail.com>
2023-10-04 23:51:38 +0900

    サポートの為の議事Rayを追加

--- Assets/MyAssets/ExampleScript.cs
+++ Assets/MyAssets/ExampleScript.cs
@@ -6,6 +6,7 @@ public class ExampleScript : MonoBehaviour
 {
     public GameObject point1; // 手に近いほうの座標を記録
     public GameObject point2; // 手から遠いほうの座標を記録
+    public GameObject fakeRay; // サポートのための議事Rayを追加
 
     void Start()
     {
